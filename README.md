b9254f7411d55c08195cc7249ae6fbc01485617a
Dom Vacchiano <djvacchiano@gmail.com>
2021-10-26 17:43:25 -0500

    init

--- README.md
+++ README.md
@@ -1,14 +1,6 @@
-# How to use this boilerplate
+# Expense Tracker
 
+Personal expense tracker for monthly budgeting
 
-1. Git clone https://github.com/vacchiano/django-launch.git
-2. `docker compose up -d --build`
-3. `docker compose exec web python manage.py migrate`
-4. Code away
-
-
-To add a new app - a personal blog for example, run 
-
-`docker compose exec web python manage.py startapp blog`
-
-Replace blog with whatever the name of the project you are starting. Also, to run any of the Django commands or pipenv install new packages you have to start with "docker compose exec web" - where exec stands for execute and web is the name of the service you defined in the docker-compose.yml file.
+### Tech Stack
+Built using Django and vanilla JS
---
+++ static/js/chart-area-demo.js
@@ -0,0 +1,54 @@
+// Set new default font family and font color to mimic Bootstrap's default styling
+Chart.defaults.global.defaultFontFamily = '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
+Chart.defaults.global.defaultFontColor = '#292b2c';
+
+// Area Chart Example
+var ctx = document.getElementById("myAreaChart");
+var myLineChart = new Chart(ctx, {
+  type: 'line',
+  data: {
+    labels: ["Jan", "Feb", "Mar", "Apr", "May", "June", "July", "Aug", "Sep", "Oct", "Nov", "Dec"],
+    datasets: [{
+      label: "Sessions",
+      lineTension: 0.3,
+      backgroundColor: "rgba(2,117,216,0.2)",
+      borderColor: "rgba(2,117,216,1)",
+      pointRadius: 5,
+      pointBackgroundColor: "rgba(2,117,216,1)",
+      pointBorderColor: "rgba(255,255,255,0.8)",
+      pointHoverRadius: 5,
+      pointHoverBackgroundColor: "rgba(2,117,216,1)",
+      pointHitRadius: 50,
+      pointBorderWidth: 2,
+      data: [3501.12, 3016.55, 3416.55, 3516.55, 3636.55, 3033.55, 3236.55, 2916.55, 3316.55, 3216.55, 2816.55, 3416.55],
+    }],
+  },
+  options: {
+    scales: {
+      xAxes: [{
+        time: {
+          unit: 'date'
+        },
+        gridLines: {
+          display: false
+        },
+        ticks: {
+          maxTicksLimit: 12
+        }
+      }],
+      yAxes: [{
+        ticks: {
+          min: 1000,
+          max: 5000,
+          maxTicksLimit: 5
+        },
+        gridLines: {
+          color: "rgba(0, 0, 0, .125)",
+        }
+      }],
+    },
+    legend: {
+      display: false
+    }
+  }
+});
