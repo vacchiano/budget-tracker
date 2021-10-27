50c0228c342af4f3cf8c721cf594e0bab3a76ef1
Dom Vacchiano <djvacchiano@gmail.com>
2021-10-26 19:47:34 -0500

    auto redirect logged in to dashboard

--- pages/views.py
+++ pages/views.py
@@ -1,7 +1,16 @@
 from django.views.generic import TemplateView
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 class HomePageView(TemplateView):
     template_name = 'home.html'
+    
+    #check if the user is logged in - if so, redirect to dashboard
+    def dispatch(self, request, *args, **kwargs):
+        if self.request.user.is_authenticated:
+            return HttpResponseRedirect(reverse('dashboard'))
+        return super(HomePageView, self).get(request, *args, **kwargs)
+
 
 class AboutPageView(TemplateView):
     template_name = 'about.html'
