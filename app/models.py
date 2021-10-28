d9573bc0ad672addf10de304702c95dea1449ae4
Dom Vacchiano <djvacchiano@gmail.com>
2021-10-28 00:33:48 -0500

    add transactions db table and display db data on dashboard page

--- app/models.py
+++ app/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+class Transaction(models.Model):
+    TYPE_CHOICES = [
+        ('Expense', 'Expense'),
+        ('Income', 'Income'),
+    ]
+    
+    CATEGORY_CHOICES = [
+        ('Housing', 'Housing'),
+        ('Transportation', 'Transportation'),
+        ('Food', 'Food'),
+        ('Utilities', 'Utilities'),
+        ('Insurance', 'Insurance'),
+        ('Medical', 'Medical'),
+        ('Savings', 'Savings'),
+        ('Personal', 'Personal'),
+        ('Entertainment', 'Entertainment'),
+        ('Misc', 'Misc'),
+        ('Income', 'Income'),
+        ]
+
+    date = models.DateField(auto_now=False, auto_now_add=False)
+    type = models.CharField(max_length=100, choices=TYPE_CHOICES)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    description = models.CharField(max_length=200, blank=True)
+    category = models.CharField(max_length=100, choices=CATEGORY_CHOICES)
+
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='transactions')
+
+    def __str__(self):
+        if self.type == "Expense":
+            return f'{self.description} | -${self.amount}'
+        else:
+            return f'{self.description} | ${self.amount}'
--- app/views.py
+++ app/views.py
@@ -1,4 +1,16 @@
+from django.contrib.auth import get_user_model
 from django.shortcuts import render
+from .models import Transaction
+from datetime import datetime
+
+
+User = get_user_model()
 
 def dashboard(request):
-    return render(request, 'app/index.html', {})
+    current_month = datetime.now().month
+    # transactions = Transaction.objects.filter(owner=request.user, date__month=current_month).order_by('-date')
+    transactions = Transaction.objects.filter(owner=request.user).order_by('-date')[:10]
+    context = {
+        'transactions': transactions
+    }
+    return render(request, 'app/index.html', context)
